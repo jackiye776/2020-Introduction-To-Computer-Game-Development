12610ff986985ad54b6e57bc7d25ac988087fec3
jackiye776 <jackieworkdrive@gmail.com>
2020-08-23 15:54:36 +1000

    70 percent complete
    Third commit

--- Assets/Scenes/LoadAssets.cs
+++ Assets/Scenes/LoadAssets.cs
@@ -6,10 +6,15 @@ public class LoadAssets : MonoBehaviour
 {
     public GameObject redObj;
 
+    [SerializeField]
+    private GameObject blueObj; 
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //transform.position = new Vector3(2.0f, 0.0f, 0.0f);
+        Instantiate(redObj, new Vector3(2, 0, 0), Quaternion.identity);
+        Instantiate(blueObj, new Vector3(-2, 0, 0), Quaternion.identity);
     }
 
     // Update is called once per frame
